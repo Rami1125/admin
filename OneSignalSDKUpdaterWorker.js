e97b85471107d8c6aa0718e960a6df50a09ae774
Rami1125 <rami.msarwa1@gmail.com>
2025-09-29 10:55:32 +0300

    Update OneSignalSDKUpdaterWorker.js

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -2,4 +2,5 @@
  * This file must be placed in the root directory of your site.
  * It is required for OneSignal push notifications to work.
  */
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+importScripts('[https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js](https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js)');
+
